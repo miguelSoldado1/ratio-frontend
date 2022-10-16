6b9b6516b659a087b12177d07cf77d0bbf2bd870
miguelsoldado1 <miguelsoldado12@gmail.com>
2022-10-16 14:36:07 +0100

    removed not needed if

--- src/components/AlbumHeader/AlbumHeader.jsx
+++ src/components/AlbumHeader/AlbumHeader.jsx
@@ -1,31 +1,25 @@
-import { useParams } from "react-router-dom";
 import { renderArtists } from "../../scripts/scripts";
 import spotifyLogo from "../../icons/spotify-logo.png";
 import "./AlbumHeader.css";
 
 export const AlbumHeader = ({ data }) => {
-  const { albumId } = useParams();
-
-  if (data?.id === albumId) {
-    return (
-      <div className="album-header">
-        <img className="album-header-image" src={data?.image} alt="" />
-        <div className="album-details-details">
-          <h1>
-            <a href={data?.album_uri}>{data?.name}</a>
-          </h1>
-          <span className="album-details-line">
-            <img src={spotifyLogo} className="spotify-logo" alt="" />
-            {renderArtists(data?.artist)}
-          </span>
-          <p className="album-details-line">
-            {handleDatePrecision(data?.release_date, data?.release_date_precision)} | {data?.tracks?.length} tracks
-          </p>
-        </div>
+  return (
+    <div className="album-header">
+      <img className="album-header-image" src={data?.image} alt="" />
+      <div className="album-details-details">
+        <h1>
+          <a href={data?.album_uri}>{data?.name}</a>
+        </h1>
+        <span className="album-details-line">
+          <img src={spotifyLogo} className="spotify-logo" alt="" />
+          {renderArtists(data?.artist)}
+        </span>
+        <p className="album-details-line">
+          {handleDatePrecision(data?.release_date, data?.release_date_precision)} | {data?.tracks?.length} tracks
+        </p>
       </div>
-    );
-  }
-  return null;
+    </div>
+  );
 };
 
 const handleDatePrecision = (release_date, release_date_precision) => {
