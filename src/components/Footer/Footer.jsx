46b8dab48902a48fee23bc5059156e6a26a7e1d1
miguelsoldado1 <miguelsoldado12@gmail.com>
2023-01-02 21:30:08 +0000

    really important change lmao

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-text">Copyright &copy; Ratio 2022</div>
+      <div className="footer-text">Copyright &copy; Ratio {new Date().getFullYear()}</div>
       <div className="footer-plugs">
         <SocialLogos />
       </div>
