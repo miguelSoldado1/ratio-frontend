e6b5e1f0bc821705c0be8095f71896437d29d714
miguelsoldado1 <miguelsoldado12@gmail.com>
2023-01-24 19:39:23 +0000

    modal ref to prevent console error

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useCallback } from "react";
+import { useRef } from "react";
 import ReactDOM from "react-dom";
 import { CSSTransition } from "react-transition-group";
 import "./Modal.css";
 
 export const Modal = ({ onClose, show, children }) => {
+  const nodeRef = useRef(null);
+
   const closeOnEscapeKeyDown = useCallback(
     (e) => {
       if ((e.charCode || e.keyCode) === 27) {
@@ -26,8 +29,8 @@ export const Modal = ({ onClose, show, children }) => {
   }, [closeOnEscapeKeyDown]);
 
   return ReactDOM.createPortal(
-    <CSSTransition in={show} unmountOnExit timeout={{ enter: 0, exit: 300 }}>
-      <div className="modal" onClick={onClose}>
+    <CSSTransition in={show} unmountOnExit timeout={{ enter: 0, exit: 300 }} nodeRef={nodeRef}>
+      <div className="modal" onClick={onClose} ref={nodeRef}>
         {children}
       </div>
     </CSSTransition>,
