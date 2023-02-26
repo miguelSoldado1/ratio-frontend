5b95da7fdade10dc0a020d6234ed47993609d304
miguelsoldado1 <miguelsoldado12@gmail.com>
2023-02-26 17:17:45 +0000

    added local storage cache to react-query

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 import { CookiesProvider } from "react-cookie";
 import App from "./App";
 import "./index.css";
 
-const queryClient = new QueryClient({ defaultOptions: { queries: { refetchOnWindowFocus: false } } });
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
+
+const persister = createSyncStoragePersister({ storage: window.localStorage });
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
+      <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
         <CookiesProvider>
           <App />
         </CookiesProvider>
         <ReactQueryDevtools />
-      </QueryClientProvider>
+      </PersistQueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
