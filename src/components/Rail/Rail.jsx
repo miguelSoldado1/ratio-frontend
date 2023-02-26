ed9a98080276f4e54a47d7ff0a63c18ab66f1966
miguelsoldado1 <miguelsoldado12@gmail.com>
2023-02-26 18:01:52 +0000

    the length thingy from last commit should really be in here

--- src/components/Rail/Rail.jsx
+++ src/components/Rail/Rail.jsx
@@ -7,14 +7,18 @@ export const Rail = ({ content }) => {
   if (isLoading) return <RailPL />;
   if (isError) return null;
 
-  return (
-    <div className="grid-container">
-      <h1 className="grid-title">{data.description}</h1>
-      <ul className="grid">
-        {data.data.map((item, index) => (
-          <Container props={item} key={index} />
-        ))}
-      </ul>
-    </div>
-  );
+  if (data.data.length > 2) {
+    return (
+      <div className="grid-container">
+        <h1 className="grid-title">{data.description}</h1>
+        <ul className="grid">
+          {data.data.map((item, index) => (
+            <Container props={item} key={index} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  return null;
 };
