6fb0df43470305233ccde1e93954c2c741fc371c
miguelsoldado1 <miguelsoldado12@gmail.com>
2023-04-25 22:24:43 +0100

    value fill in

--- src/components/RatingCircleV2/RatingCircle.jsx
+++ src/components/RatingCircleV2/RatingCircle.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CircularProgressBar from "./CircularProgressBar";
 import "./RatingCircle.css";
 
 export const RatingCircle = ({ value, description, variant }) => {
+  const [renderValue, setRenderValue] = useState(0);
+
+  useEffect(() => {
+    if (value) renderValue <= 0 ? setTimeout(() => setRenderValue(value), 250) : setRenderValue(value);
+    return () => setRenderValue(0);
+  }, [value, renderValue]);
+
   return (
     <div className="rating-circle-container">
-      <CircularProgressBar value={value ?? 0} variant={variant}>
+      <CircularProgressBar value={renderValue ?? 0} variant={variant}>
         <div className="rating-circle-wrapper">
           <h1>{value !== null && value >= 0 ? value : "-"}</h1>
           {description && <p>{description}</p>}
