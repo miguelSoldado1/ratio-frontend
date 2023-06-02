9788b29507b0dfb8ad88c9368c0b7a664df3959f
MIGUEL SOLDADO <miguel-a-soldado@telecom.pt>
2023-06-02 16:26:20 +0100

    improved search bar + fixed eventListeners

--- src/components/NavigationBar/SearchBar/SearchBar.jsx
+++ src/components/NavigationBar/SearchBar/SearchBar.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import useDebounce from "../../../hooks/useDebounce";
 import { searchForAlbum } from "../../../api/navigationBar";
 import useAccessToken from "../../../hooks/useAuthentication";
-import { ReactComponent as SearchIcon } from "../../../icons/search-icon.svg";
 import { SearchResult } from "../../";
+import { ReactComponent as SearchIcon } from "../../../icons/search-icon.svg";
 import "./SearchBar.css";
 
 export const SearchBar = () => {
   const { accessToken } = useAccessToken();
   const [searchQuery, setSearchQuery] = useState("");
   const debouncedSearchQuery = useDebounce(searchQuery, 300);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleMouseDown);
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("mousedown", handleMouseDown);
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
   const { data, isLoading } = useQuery({
     queryKey: ["search", debouncedSearchQuery],
@@ -19,9 +29,10 @@ export const SearchBar = () => {
   });
 
   return (
-    <div className="search">
+    <div className="search" data-backdrop="static">
       <SearchIcon className="search-icon" />
       <input
+        id="search"
         type="search"
         className="search-input"
         autoComplete="off"
@@ -34,11 +45,19 @@ export const SearchBar = () => {
   );
 };
 
-document.addEventListener("mousedown", (event) => {
+const handleMouseDown = (event) => {
   const searchElement = document.querySelector(".search");
   const searchResultElement = document?.querySelector(".search-result");
   if (searchElement && searchResultElement) {
     if (searchElement.contains(event.target)) searchResultElement.style.display = "block";
     else searchResultElement.style.display = "none";
   }
-});
+};
+
+const handleKeyDown = (event) => {
+  if (event.ctrlKey && event.key === "k") {
+    event.preventDefault();
+    const searchBar = document.getElementById("search");
+    searchBar.focus();
+  }
+};
