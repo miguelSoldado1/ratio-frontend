c1fd90eb4da36e39c95754c24aca864e3e73ae24
miguel <miguelsoldado12@gmail.com>
2023-06-24 16:51:23 +0100

    tweaks to home ratings

--- src/components/HomeRating/HomeRating.jsx
+++ src/components/HomeRating/HomeRating.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { HomeRatingLikes } from "./HomeRatingLikes/HomeRatingLikes";
 import { HomeRatingAlbum } from "./HomeRatingAlbum/HomeRatingAlbum";
 import { HomeRatingAvatar } from "./HomeRatingAvatar/HomeRatingAvatar";
@@ -7,14 +8,14 @@ import "./HomeRating.css";
 
 const HomeRating = (post) => {
   return (
-    <div className="rating-container">
+    <Link to={`/album/${post.album_id}`} className="rating-container">
       <HomeRatingAvatar user={post.user}>
         <span className="created-date overflow-ellipsis">{handleDate(post.createdAt)}</span>
       </HomeRatingAvatar>
       <HomeRatingAlbum album={post.album} />
       <HomeRatingPost post={post} />
       <HomeRatingLikes likes={post.likes} ratingId={post._id} likedByUser={post.liked_by_user} />
-    </div>
+    </Link>
   );
 };
 
--- src/components/HomeRating/HomeRatingAvatar/HomeRatingAvatar.jsx
+++ src/components/HomeRating/HomeRatingAvatar/HomeRatingAvatar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import "./HomeRatingAvatar.css";
 import { Link } from "react-router-dom";
+import avatarPlacehoder from "../../../icons/avatar-placeholder.svg";
+
+import "./HomeRatingAvatar.css";
 
 export const HomeRatingAvatar = ({ user, children }) => {
   return (
     <div className="header-container">
       <div className="avatar-container">
-        <img src={user.imageUrl} alt={user.displayName} />
+        <img src={user.imageUrl ?? avatarPlacehoder} alt={user.displayName} />
         <Link to={`/profile/${user.id}`} className="overflow-ellipsis">
           {user.displayName}
         </Link>
