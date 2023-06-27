38b7d159220cb3b66883766ab102fcf4bd8ec3ea
miguel <miguelsoldado12@gmail.com>
2023-06-27 20:56:17 +0100

    links on top of another link removed

--- src/components/HomeRating/HomeRating.jsx
+++ src/components/HomeRating/HomeRating.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { HomeRatingLikes } from "./HomeRatingLikes/HomeRatingLikes";
 import { HomeRatingAlbum } from "./HomeRatingAlbum/HomeRatingAlbum";
 import { HomeRatingAvatar } from "./HomeRatingAvatar/HomeRatingAvatar";
@@ -8,14 +7,14 @@ import "./HomeRating.css";
 
 const HomeRating = (post) => {
   return (
-    <Link to={`/album/${post.album_id}`} className="rating-container">
+    <div className="rating-container">
       <HomeRatingAvatar user={post.user}>
         <span className="created-date overflow-ellipsis">{handleDate(post.createdAt)}</span>
       </HomeRatingAvatar>
       <HomeRatingAlbum album={post.album} />
       <HomeRatingPost post={post} />
       <HomeRatingLikes likes={post.likes} ratingId={post._id} likedByUser={post.liked_by_user} />
-    </Link>
+    </div>
   );
 };
 
