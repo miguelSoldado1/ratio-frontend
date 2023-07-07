79a64bb9194a26bd157600a2a69808ca576470c4
miguel <miguelsoldado12@gmail.com>
2023-07-07 22:40:10 +0100

    scroll down button improvements

--- src/components/ScrollDownButton/ScrollDownButton.jsx
+++ src/components/ScrollDownButton/ScrollDownButton.jsx
@@ -1,36 +1,43 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Button } from "..";
 import "./ScrollDownButton.css";
 
 export const ScrollDownButton = ({ textAreaRef }) => {
-  const [clicked, setClicked] = useState(false);
+  const [showButton, setShowButton] = useState(true);
+  const observerRef = useRef(null);
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 600) setClicked(true);
-    });
-
-    return () => {
-      window.removeEventListener("scroll", () => {
-        if (window.scrollY > 600) setClicked(false);
+    const handleIntersection = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setShowButton(false);
+          observerRef.current.disconnect();
+        }
       });
     };
-  }, []);
+
+    observerRef.current = new IntersectionObserver(handleIntersection, { root: null, rootMargin: "150px", threshold: 0 });
+    observerRef.current.observe(textAreaRef.current);
+
+    return () => {
+      if (observerRef.current) {
+        observerRef.current.disconnect();
+      }
+    };
+  }, [textAreaRef]);
 
   const handleClick = () => {
-    setClicked(true);
+    setShowButton(false);
     textAreaRef?.current?.scrollIntoView({ behavior: "smooth", block: "center" });
   };
 
+  if (!showButton || !textAreaRef) {
+    return null;
+  }
+
   return (
-    <>
-      {!clicked && textAreaRef ? (
-        <div className="scroll-down-button">
-          <Button onClick={handleClick}>Give it a rating</Button>
-        </div>
-      ) : null}
-    </>
+    <div className="scroll-down-button">
+      <Button onClick={handleClick}>Give it a rating</Button>
+    </div>
   );
 };
