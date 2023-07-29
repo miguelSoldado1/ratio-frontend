c90f8a3e24fa88174df5f5e744c320d452893abd
miguel <miguelsoldado12@gmail.com>
2023-07-29 19:07:25 +0100

    wasn't using the pl's in home screen for some reason

--- src/components/PostsFeed/PostsFeed.jsx
+++ src/components/PostsFeed/PostsFeed.jsx
@@ -18,15 +18,11 @@ export const PostsFeed = ({ userId }) => {
     enabled: !!userId,
   });
 
-  if (isInitialLoading || !posts) {
-    return null;
-  }
-
   return (
     <div className="posts-feed-container">
-      {posts
-        ? posts.pages.map((page) => page.data.map((post) => <PostRating {...post} key={post._id} />))
-        : [...Array(4)].map((_, index) => <PostRatingPL key={index} />)}
+      {!posts || isInitialLoading
+        ? [...Array(4)].map((_, index) => <PostRatingPL key={index} />)
+        : posts.pages.map((page) => page.data.map((post) => <PostRating {...post} key={post._id} />))}
       {hasNextPage && <Loading fetchNextPage={fetchNextPage} />}
     </div>
   );
