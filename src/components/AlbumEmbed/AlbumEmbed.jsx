0c0c390fd1cf81d264527f43a0b09b39714c57e6
miguel <miguelsoldado12@gmail.com>
2023-07-31 22:09:06 +0100

    album embed updated

--- src/components/AlbumEmbed/AlbumEmbed.jsx
+++ src/components/AlbumEmbed/AlbumEmbed.jsx
@@ -8,7 +8,7 @@ export const AlbumEmbed = ({ albumId }) => {
     <div className="album-spotify-embed-container gradient">
       <iframe
         title="Spotify Album Embed"
-        src={`https://open.spotify.com/embed/album/${albumId}?utm_source=generator&theme=0`}
+        src={`https://open.spotify.com/embed/album/${albumId}?theme=0`}
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         allowFullScreen
         className={visibility ? "visible" : "hidden"}
