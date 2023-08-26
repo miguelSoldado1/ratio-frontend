c2c747dbfc724a7f2874332c231f3db647f4ebf4
miguel <miguelsoldado12@gmail.com>
2023-08-26 14:11:03 +0100

    added generatesourcempa to vite.config taking env variable into consideration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import eslint from "vite-plugin-eslint";
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
   return {
     build: {
       outDir: "build",
+      sourcemap: env.GENERATE_SOURCE_MAP === "true",
     },
     plugins: [react(), svgr(), eslint()],
   };
